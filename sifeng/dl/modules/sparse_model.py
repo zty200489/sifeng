a7a50710a17ae5f7a692e4658ae8c73017b40127
zty200489 <zty200489@gmail.com>
2023-10-21 12:18:03 +0800

    perf: improved moe performance

--- sifeng/dl/modules/sparse_model.py
+++ sifeng/dl/modules/sparse_model.py
@@ -1,11 +1,22 @@
-import torch, numpy as np
+import torch
 from torch import nn
-from torch.nn.parameter import Parameter
-from torch.nn.init import xavier_uniform_
 from .transformers import FeedForwardLayer
+
+from typing import Optional, Callable
 
 class MixtureOfExpertsBlock(nn.Module):
     """The Mixture of Experts Module
+    - MoE
+      N. Shazeer et al., “Outrageously Large Neural Networks: The Sparsely-Gated Mixture-of-Experts
+      Layer.” arXiv, Jan. 23, 2017. Accessed: Sep. 27, 2023. [Online]. Available:
+      http://arxiv.org/abs/1701.06538
+    - FastMoE
+      J. He, J. Qiu, A. Zeng, Z. Yang, J. Zhai, and J. Tang, “FastMoE: A Fast Mixture-of-Expert Tra
+      ining System.” arXiv, Mar. 24, 2021. Accessed: Sep. 28, 2023. [Online]. Available:
+      http://arxiv.org/abs/2103.13262
+    - TODO: ST-MoE
+    B. Zoph et al., “ST-MoE: Designing Stable and Transferable Sparse Expert Models.” arXiv, Apr.
+    29, 2022. Accessed: Sep. 27, 2023. [Online]. Available: http://arxiv.org/abs/2202.08906
 
     Parameters
     ----------
@@ -15,6 +26,8 @@ class MixtureOfExpertsBlock(nn.Module):
         the number of expert modules
     topk: int, default `2`
         the number of topk experts to activate
+    gate: Optional[Callable[[torch.Tensor], torch.Tensor]], default `None`
+        the gate layer, if `None` default to simple linear transformation
     expert: torch.nn.Module, default `sifeng.dl.FeedForwardLayer`
         the choice of expert module
     *args, **kwargs:
@@ -29,6 +42,7 @@ class MixtureOfExpertsBlock(nn.Module):
                  embed_dim: int,
                  num_experts: int,
                  topk: int = 2,
+                 gate: Optional[Callable[[torch.Tensor], torch.Tensor]] = None,
                  expert: nn.Module = FeedForwardLayer,
                  *args, **kwargs) -> None:
         super(MixtureOfExpertsBlock, self).__init__()
@@ -37,21 +51,25 @@ class MixtureOfExpertsBlock(nn.Module):
         self.topk = topk
 
         self.softmax = nn.Softmax(dim=-1)
-        self.W_gate = Parameter(torch.empty(embed_dim, self.num_experts))
+        self.gate = gate if gate is not None else nn.Linear(embed_dim, self.num_experts)
         self.experts = nn.ModuleList([expert(embed_dim=embed_dim, *args, **kwargs) for _ in range(self.num_experts)])
-
-        self._init_params()
-
-    def _init_params(self):
-        xavier_uniform_(self.W_gate)
 
     def forward(self,
                 x: torch.Tensor, # [bsz * ?? * embed_dim]
                 ) -> torch.Tensor:
-        gate = torch.matmul(x, self.W_gate) # [bsz * ?? * num_experts]
-        kth = torch.min(torch.topk(gate, k=self.topk, dim=-1, sorted=False).values, dim=-1, keepdim=True).values # [bsz * ?? * 1]
-        mask = self.softmax(torch.where(torch.ge(gate, kth), gate, -np.inf)) # [bsz * ?? * num_experts]
-        x = x.unsqueeze(dim=-2).repeat(1, 1, self.num_experts, 1) # [bsz * ?? * num_experts * embed_dim]
-        x = torch.concat([self.experts[i](x.select(dim=-2, index=i).unsqueeze(dim=-2)) for i in range(self.num_experts)], dim=-2) # [bsz * ?? * num_experts * embed_dim]
-        x = torch.mul(x, mask.unsqueeze(dim=-1)) # [bsz * ?? * num_experts * embed_dim]
-        return x.mean(dim=-2) # [bsz * ?? * embed_dim]
+        device = x.device
+        fsz = x.shape[:-1] # fsz = [bsz * ??]
+        x = x.flatten(start_dim=0, end_dim=-2) # [fsz * embed_dim]
+        gate = self.gate(x) # [fsz * num_experts]
+        topk_values, topk_indices = torch.topk(gate, k=self.topk, dim=-1, sorted=True) # [fsz * topk], [fsz * topk]
+        topk_weight = self.softmax(topk_values) # [fsz * topk]
+        mask = torch.zeros([topk_weight.shape[0], self.num_experts], device=device, dtype=topk_weight.dtype).scatter(dim=1, index=topk_indices, src=topk_weight) # [fsz * num_experts]
+        activators = [mask[:, i].nonzero().squeeze(-1) for i in range(self.num_experts)] # List([?], len=num_experts)
+        weights = [mask[:, i].index_select(dim=0, index=activators[i]) for i in range(self.num_experts)]
+        inputs = [x.index_select(dim=0, index=activators[i]) for i in range(self.num_experts)] # List([? * embed_dim], len=num_experts)
+        outputs = [torch.mul(self.experts[i](inputs[i]), weights[i].unsqueeze(dim=-1)) for i in range(self.num_experts)] # List([? * embed_dim], len=num_experts)
+        x = torch.zeros(x.shape[0], outputs[0].shape[-1], device=device, dtype=x.dtype) # [fsz * embed_dim]
+        for i in range(self.num_experts):
+            x = x.scatter_add(0, activators[i].unsqueeze(dim=-1).repeat_interleave(outputs[0].shape[-1], dim=-1), outputs[i]) # [fsz * embed_dim]
+        x = x.unflatten(dim=0, sizes=fsz) # [bsz * ?? * embed_dim]
+        return x
